aa31463d6f9499238e32a91bd7f53a310451a1c2
Cameron <horst106@mail.chapman.edu>
2019-04-04 19:54:20 -0700

    sa fdfsd a

--- scripts/main.js
+++ scripts/main.js
@@ -53,7 +53,7 @@ function animate() {
     window.requestAnimationFrame( animate );
     controls.update();
     renderer.render( scene, camera );
-    document.getElementById("100").innerHTML = "v3:    " + JSON.stringify(camera.rotation);
+    document.getElementById("100").innerHTML = "v3:    " + JSON.stringify(controls.deviceOrientation);
 }
 
 function onWindowResize() {
